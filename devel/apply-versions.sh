9fd60533776cfd7b88f75965d3255aa88880314a
Martchus <martchus@gmx.net>
2020-10-20 10:57:38 +0200

    WIP: Update mingw-w64-qt6-* to beta1

--- devel/apply-versions.sh
+++ devel/apply-versions.sh
@@ -20,8 +20,11 @@ for pkgbuild_file in "${PKGBUILD_DIR:-.}"/*/*/PKGBUILD; do
        || $variant      == 'mingw-w64-test'     # just our own 'test' package (not used anymore)
     ]] && continue
 
-    # treat all qt5-* packages as qt5
-    [ ${project_name%%-*} == 'qt5' ] && project_name='qt5'
+    # treat all qt5-*/qt6-* packages as qt5/qt6
+    start=${project_name%%-*}
+    for qtver in 5 6; do
+        [[ $start  == "qt$qtver" ]] && project_name="qt$qtver" && break
+    done
 
     # skip packages with unknown version
     version=${versions[$project_name]}
@@ -38,7 +41,7 @@ for pkgbuild_file in "${PKGBUILD_DIR:-.}"/*/*/PKGBUILD; do
     [[ -f $template ]] && pkgbuild_file=$template
 
     # apply new version
-    sed -i -e "s/^pkgver=.*/pkgver=$version/" -e "s/pkgrel=.*/pkgrel=1/" "$pkgbuild_file"
+    sed -i -e "s/^\(_qtver\|pkgver\)=[^\$].*/\1=$version/" -e "s/pkgrel=.*/pkgrel=1/" "$pkgbuild_file"
     chmod 644 "$pkgbuild_file"
 
     echo "$trimmed_path -> $version"
